35b181e7ca7b493e2d2d4658059181abba8e9f48
Tosox <57193602+Tosox@users.noreply.github.com>
2025-10-13 16:18:04 +0200

    docs: Add explanation for extension code

--- extensions/ticktick/src/main/java/de/tosox/revanced/extension/ticktick/pro/UnlockProPatch.java
+++ extensions/ticktick/src/main/java/de/tosox/revanced/extension/ticktick/pro/UnlockProPatch.java
@@ -5,6 +5,7 @@ import com.ticktick.task.data.User;
 @SuppressWarnings("unused")
 public final class UnlockProPatch {
 	public static boolean shouldBePro(User user) {
+		// Bypass 'new User().isPro()' detection
 		return user.getUsername() != null;
 	}
 }
